b78798957fc093f1db93f0ed0a028583de25349c
Psyadav77 <1996prabhat@gmail.com>
2023-04-26 21:47:07 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 import './style.css';
 
 // Write Javascript code!
+//callback is a function that when we are going to call when the result async operation is ready
+
+console.log('Before');
+  getUser(1,function callback(user){
+    console.log(user);
+  })
+console.log("After");
+
+
+function getUser(id,callBack)
+{
+  setTimeout(()=>{
+    console.log('Data Rread From DataBase');
+    callBack({id:id,userName:'Prabhat'});
+  },2000)
+}
