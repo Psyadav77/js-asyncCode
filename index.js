13eb84dc8f4966627211e71a992d885b7be11563
Psyadav77 <1996prabhat@gmail.com>
2023-04-26 23:07:59 +0530

    add one more callback function

--- index.js
+++ index.js
@@ -5,16 +5,26 @@ import './style.css';
 //callback is a function that when we are going to call when the result async operation is ready
 
 console.log('Before');
-  getUser(1,function callback(user){
-    console.log(user);
-  })
-console.log("After");
+getUser(1, function callback(user) {
+  //console.log(user);
+  getRepo(user.userName, (repo) => {
+    console.log(repo);
+  });
 
+});
+//get the Repositories
 
-function getUser(id,callBack)
-{
-  setTimeout(()=>{
+console.log('After');
+
+function getUser(id, callBack) {
+  setTimeout(() => {
     console.log('Data Rread From DataBase');
-    callBack({id:id,userName:'Prabhat'});
-  },2000)
+    callBack({ id: id, userName: 'Prabhat' });
+  }, 2000);
+}
+
+function getRepo(userName, callback) {
+  setTimeout(() => {
+    callback(['repo1', 'repo2', 'repo3']);
+  }, 2000);
 }
