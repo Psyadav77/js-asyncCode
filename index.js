e319d17b482f98311c2d838c75fce478060839d6
Psyadav77 <1996prabhat@gmail.com>
2023-04-27 06:48:37 +0530

    Promise Created

--- index.js
+++ index.js
@@ -2,6 +2,15 @@
 import './style.css';
 
 // Write Javascript code!
-//callback is a function that when we are going to call when the result async operation is ready
+//promise holds the eventual result on an async operation
 
-console.log('Promise');
+const p = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    //  resolve(1);
+    reject(new Error('Error mesg'));
+  }, 2000);
+});
+
+p.then((result) => console.log('Result', result)).catch((err) =>
+  console.log('Error->', err.message)
+);
